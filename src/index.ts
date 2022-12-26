640b9b5efc624aa6fd698c5553b9ed8e531db85b
Joseley Rebelo <joseleyrebelo@Joseleys-MacBook-Pro.local>
2022-12-26 04:18:26 +0000

    Improves on making package installable

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./context/FormContext";
+export * from "./components/Input";
+export * from "./components/InputEmail";
+export * from "./components/InputPassword";
+export * from "./components/Submit";
